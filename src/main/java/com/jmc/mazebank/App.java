f97e98dc0b3182e085fb5a8aed82dd729e2b5db4
davidleonmayor <jd.leon@unimayor.edu.co>
2024-11-18 21:31:36 -0500

    module-info content and remane packages gith upper camel case

--- src/main/java/com/jmc/mazebank/App.java
+++ src/main/java/com/jmc/mazebank/App.java
@@ -1,4 +1,7 @@
 package com.jmc.mazebank;
 
 public class App {
+    public static void main(String[] args) {
+
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,16 @@
 module com.david.stydyapp.studyapp {
     requires javafx.controls;
     requires javafx.fxml;
-    // requires javafx.web;
-
-    requires org.controlsfx.controls;
-    // requires com.dlsc.formsfx;
-    // requires net.synedra.validatorfx;
-    // requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    // requires eu.hansolo.tilesfx;
-    // requires com.almasb.fxgl.all;
+    requires de.jensd.fx.glyphs.fontawesome;
+    requires java.sql;
+    requires org.xerial.sqlitejdbc;
 
     opens com.jmc.mazebank to javafx.fxml;
+    
     exports com.jmc.mazebank;
+    exports com.jmc.mazebank.Controllers;
+    exports com.jmc.mazebank.Controllers.Admin;
+    exports com.jmc.mazebank.Controllers.Client;
+    exports com.jmc.mazebank.Models;
+    exports com.jmc.mazebank.Views;
 }
