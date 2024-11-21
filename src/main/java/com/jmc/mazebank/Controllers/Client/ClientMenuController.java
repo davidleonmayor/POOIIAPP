0ecd7038ede6e9e1264e3a542b51da5ec6431fb2
davidleonmayor <jd.leon@unimayor.edu.co>
2024-11-21 10:56:16 -0500

    dashboard fxml and some styles

--- src/main/java/com/jmc/mazebank/Controllers/Client/ClientMenuController.java
+++ src/main/java/com/jmc/mazebank/Controllers/Client/ClientMenuController.java
@@ -1,4 +1,20 @@
 package com.jmc.mazebank.Controllers.Client;
 
-public class ClientMenuController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientMenuController implements Initializable {
+    @FXML
+    public Button dashboard_btn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+    // @FXML
+
 }
